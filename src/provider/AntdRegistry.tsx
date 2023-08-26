4eaac6a9a548bd79fd25973fe311c3b6d9c250f0
Nguyen Phi Nam <nguyenphinam2k2@gmail.com>
2023-08-26 11:09:13 +0700

    fix bug provider style antd

--- src/provider/AntdRegistry.tsx
+++ src/provider/AntdRegistry.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs';
 import { useServerInsertedHTML } from 'next/navigation';
 
+const cache = createCache();
 const StyledComponentsRegistry = ({ children }: { children: React.ReactNode }) => {
-  const cache = createCache();
   useServerInsertedHTML(() => (
     <style id="antd" dangerouslySetInnerHTML={{ __html: extractStyle(cache, true) }} />
   ));
