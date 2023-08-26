f0a9edc817d0d1941cecc6235d41ca09649eebca
Nguyen Phi Nam <nguyenphinam2k2@gmail.com>
2023-08-26 11:05:49 +0700

    add provider antd nextjs

--- src/app/myLoan/page.tsx
+++ src/app/myLoan/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './MyLoan.scss'
+import { Avatar, Segmented } from 'antd'
+import { UserOutlined } from '@ant-design/icons'
 
 
 const myLoan = () => {
@@ -7,6 +9,28 @@ const myLoan = () => {
     <div className='app-myloan'>
       <p className='app-myloan--header'>Your Loan</p>
 
+      <Segmented
+        options={[
+          {
+            label: (
+              <div className='segment-item'>
+                <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel" />
+                <div className='name'>Borrow</div>
+              </div>
+            ),
+            value: 'user1',
+          },
+          {
+            label: (
+              <div className='segment-item'>
+                <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+                <div className='name'>Lend</div>
+              </div>
+            ),
+            value: 'user2',
+          },
+        ]}
+      />
     </div>
   )
 }
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,13 +2,14 @@
 
 'use client'
 import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
-import { renderToString } from 'react-dom/server';
-import { ThemeProvider } from 'next-themes'
 import { toast, ToastContainer } from "react-toastify";
 import store, {persistor} from '@/state';
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
 import 'react-toastify/dist/ReactToastify.css';
+import StyledComponentsRegistry from '@/provider/AntdRegistry';
+
+
 
 export function Providers({ children } : {
     children: React.ReactNode
@@ -20,13 +21,15 @@ export function Providers({ children } : {
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <StyleProvider cache={cache}>
-          {/* <ThemeProvider> */}
-            {children}
-          {/* </ThemeProvider> */}
+          <StyledComponentsRegistry>
+            {/* <ThemeProvider> */}
+              {children}
+            {/* </ThemeProvider> */}
+          </StyledComponentsRegistry>
         </StyleProvider>
       </PersistGate>
       <ToastContainer 
-              position="bottom-center"
+          position="bottom-center"
               autoClose={5000}
               hideProgressBar={false}
               newestOnTop={false}
